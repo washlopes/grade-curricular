731d807e5f29112c070944babb109e34e8e27c11
washlopes <washlopes@gmail.com>
2021-02-24 09:41:08 -0300

    Commit inicial

---
+++ src/test/java/br/com/vector/gradecurricular/GradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vector.gradecurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
